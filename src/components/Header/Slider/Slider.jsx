b696f5efe7bcee467dbe7b225084040225d2cf73
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-10-18 07:02:10 +0600

    Bannar Section added here

--- src/components/Header/Slider/Slider.jsx
+++ src/components/Header/Slider/Slider.jsx
@@ -1,21 +1,21 @@
 import banner from "../../../assets/banner.jpg"
 const Slider = () => {
     return (
-      <div className="bg-[#262626] text-white flex h-96 px-5 md:px-16 pt-7 space-x-5">
-        <div className="w-2/3">
-          <h1>
+      <div className="bg-[#262626] text-white lg:flex py-7 items-center px-5 md:px-16 pt-7 ">
+        <div className="lg:w-2/3 mb-7 text-center lg:text-left space-y-5  items-center">
+          <h1 className="text-5xl font-extrabold">
             GOOD FOOD MAKE <br />
             <span className="text-[#c5a35e]">FRESH MIND</span>
           </h1>
-          <p>
+          <p className="md:w-3/4 text-gray-300">
             Rustle up an easy veggie traybake for a quick midweek dinner,
             perfect for feeding the family with minimal washing up.
           </p>
-          <button>Choose Menu</button>
+          <button className="btn rounded-none bg-[#c5a35e] text-white hover:bg-white hover:text-black text-xs">Choose Menu</button>
         </div>
 
-        <div className="">
-            <img className="rounded-full w-80" src={banner} alt="" />
+        <div className="flex md:justify-center lg:justify-start">
+          <img className="rounded-full w-full md:w-96" src={banner} alt="" />
         </div>
       </div>
     );
